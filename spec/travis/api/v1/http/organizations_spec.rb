9c36483fb9368eab33f191f32f813d2e4e3ae68d
Josh Kalderimis <josh.kalderimis@gmail.com>
2013-07-06 02:55:42 +0200

    some more fixes, good times

--- spec/travis/api/v1/http/organizations_spec.rb
+++ spec/travis/api/v1/http/organizations_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe Travis::Api::V1::Http::Organizations do
   include Travis::Testing::Stubs
 
-  let(:data) { Travis::Api::V1::Http::Organizations.new([organization]).data }
+  let(:data) { Travis::Api::V1::Http::Organizations.new([org]).data }
 
   it 'data' do
     data.first.should == {
-      'id' => organization.id,
-      'name' => organization.name,
-      'login' => organization.login,
+      'id' => org.id,
+      'name' => org.name,
+      'login' => org.login,
     }
   end
 end
